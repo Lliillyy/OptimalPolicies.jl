74ef74c96cc2b9532ece611d8af5415768479b5a
Yuqing Pan <yuqingpan@g.harvard.edu>
2023-07-05 15:58:55 -0700

    Added U and curried to foo.jl

--- src/foo.jl
+++ src/foo.jl
@@ -7,3 +7,22 @@ Creates a 2-element static array from the scalars `x` and `y`.
 function foo(x::Number, y::Number)
     SA[x, y]
 end
+
+"""
+    U(gam, x)
+
+The potential function for some given potential barrier height `gam`
+"""
+function U(gam::Number, x::Number)
+    gam * (x * x - 1) * (x * x - 1)
+end
+
+"""
+    curried(gam)
+
+Returns a potential function for the given potential barrier height `gam`
+"""
+function curried(gam::Number)
+    x -> U(gam, x)
+end
+
