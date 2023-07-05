3e7afa3a95157b5a10ec555bb2fb82f55e101be9
Yuqing Pan <yuqingpan@g.harvard.edu>
2023-07-04 18:15:43 -0700

    Added make.jl

---
+++ docs/make.jl
@@ -0,0 +1,16 @@
+using Documenter
+using OptimalPolicies
+
+makedocs(
+    sitename = "OptimalPolicies.jl",
+    format = Documenter.HTML(prettyurls = false),
+    pages = [
+        "Introduction" => "index.md",
+        "API" => "api.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/Lliillyy/OptimalPolicies.jl.git",
+    devbranch = "main"
+)
