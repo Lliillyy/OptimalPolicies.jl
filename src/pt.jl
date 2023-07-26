1f04efe77826f269b068ac4a2723b0b9c983bc8d
Yuqing Pan <yuqingpan@g.harvard.edu>
2023-07-26 14:07:03 -0400

    update pt to align with Pigeons v0.2.0

--- src/pt.jl
+++ src/pt.jl
@@ -1,5 +1,5 @@
 using Pigeons
-using SplittableRandoms
+using Random
 import Base.@kwdef
 using Distributions
 
@@ -22,12 +22,12 @@ function recompute_U(state::UState)
     return (state.x^2 - 1)^2
 end
 
-# sample all binary variable i.i.d. Bern(1/2)
+# sample
 function iid_bb!(state::UState)
     @assert recompute_U(state) == state.U_value
-    alpha = 0.2    # Shape parameter of the Beta distribution
-    beta = 0.25     # Shape parameter of the Beta distribution
-    n = 10         # Number of trials
+    alpha = 2   # Shape parameter of the Beta distribution
+    beta = 2     # Shape parameter of the Beta distribution
+    n = 20         # Number of trials
     bb_dist = BetaBinomial(n, alpha, beta)
     state.x = rand(bb_dist)
     state.U_value = recompute_U(state)
@@ -39,7 +39,7 @@ end
 (log_potential::ULogPotential)(state::UState) = log_potential.beta * state.U_value
 
 # Reference distribution uses beta = 0...
-Pigeons.create_reference_log_potential(log_potential::ULogPotential, ::Inputs) = ULogPotential(0.0)
+Pigeons.default_reference(log_potential::ULogPotential) = ULogPotential(0.0)
 # ... so that we can do i.i.d. sampling of Bernoullis at the reference:
 function Pigeons.sample_iid!(reference_log_potential::ULogPotential, replica, shared)
     @assert reference_log_potential.beta == 0.0
@@ -47,8 +47,7 @@ function Pigeons.sample_iid!(reference_log_potential::ULogPotential, replica, sh
 end
 
 # Initialization: all entries to zeros (falses)
-Pigeons.create_state_initializer(my_potential::ULogPotential, ::Inputs) = my_potential
-Pigeons.initialization(log_potential::ULogPotential, ::SplittableRandom, ::Int) = UState(0.0)
+Pigeons.initialization(log_potential::ULogPotential, ::AbstractRNG, ::Int) = UState(0.0)
 
 # MCMC explorer 
 # This struct should not contain state that is replica-specific 
@@ -62,9 +61,9 @@ Pigeons.default_explorer(lp::ULogPotential) = UMetropolis()
 
 # Perform explorer MCMC step
 function Pigeons.step!(explorer::UMetropolis, replica, shared)
-    alpha = 0.2    # Shape parameter of the Beta distribution
-    beta = 0.25     # Shape parameter of the Beta distribution
-    n = 10         # Number of trials
+    alpha = 2    # Shape parameter of the Beta distribution
+    beta = 2     # Shape parameter of the Beta distribution
+    n = 20         # Number of trials
     bb_dist = BetaBinomial(n, alpha, beta)
     # Note: the log_potential is an InterpolatedLogPotential between two IsingLogPotential's
     log_potential = Pigeons.find_log_potential(replica, shared.tempering, shared)
@@ -82,19 +81,19 @@ function Pigeons.step!(explorer::UMetropolis, replica, shared)
             replica.state.x = prev_x
             replica.state.U_value = recompute_U(replica.state)
         end # (nothing to do if accept, we work in-place)
-end
+    end
 end
 
 # perform sampling - sanity check: log(Z) ≈ true value of around 33.3 for this example
-pt = pigeons(target = ULogPotential(1.0), recorder_builders = [index_process])
+pt = pigeons(target = ULogPotential(1.0), record = [index_process])
 
 # # sanity check: the local communication barrier has a peak near the predicted phase transition log(1+sqrt(2))/2
-# import Pkg; Pkg.add("Plots")
 using Plots
 
 plot2 = plot(pt.reduced_recorders.index_process);
 savefig(plot2, "U_index_process_plot.png");
 
-# plotlyjs()
+# plotlyjs() this line creates error
 plot1 = plot(pt.shared.tempering.communication_barriers.localbarrier)
 savefig(plot1, "U_localbarrier.png")
+
